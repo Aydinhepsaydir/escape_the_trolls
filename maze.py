28bed90a300db95deef06e8919c28f9e731f41e5
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2017-12-08 22:09:23 +0000

    Update maze.py
    maze is now inside array and prints perfectly

--- maze.py
+++ maze.py
@@ -1,13 +1,18 @@
 def build_maze():
 
-	array = []
-	with open("data/maze_structure.txt", "r") as text:
-		for line in text:
-			maze_wall = text.readline()
-			array.append(maze_wall.split())
+	array = [] 
 
-	for i in array:
-		print(i)
+	with open("data/maze_structure.txt", "r") as data:
+
+		while True:
+
+			x = data.readline()
+			array.append(list(x))
+
+			if not x: break
+
+	for row in array:
+		print(" ".join(map(str, row)))
 
 if __name__ == '__main__':
     build_maze()
